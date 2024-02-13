9a1e47a158dc2fef2bf279e662f657141d06c90c
sanjaibalajee <87613424+sanjaibalajee@users.noreply.github.com>
2024-02-13 19:49:48 +0530

    Initial Commit

---
+++ skedaX/skedaXApp.swift
@@ -0,0 +1,17 @@
+//
+//  skedaXApp.swift
+//  skedaX
+//
+//  Created by Sanjai Balajee on 13/02/24.
+//
+
+import SwiftUI
+
+@main
+struct skedaXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
